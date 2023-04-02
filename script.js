4ca6c1f759a84b1dd77fa46d716476838e62ce05
Emanuel Nogueira <manunogueira8814@gmail.com>
2023-04-02 20:01:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const box = document.querySelector(".container");
+const imagens = document.querySelectorAll(".container img");
+
+let contador = 0;
+
+function slider() {
+  contador++;
+
+  if (contador > imagens.length - 1) {
+    contador = 0;
+  }
+
+  box.style.transform = `translateX(${-contador * 1250}px)`;
+}
+
+setInterval(slider, 2000);
